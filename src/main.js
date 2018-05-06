3dbaf194616d16696b49d0d5b8e21bce0b1d0355
Alexander Levin <alevin.ru@gmail.com>
2018-05-06 18:25:53 +0300

    mint

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import 'es6-promise/auto';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import locale from 'element-ui/lib/locale/lang/ru-RU';
+
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+// import locale from 'element-ui/lib/locale/lang/ru-RU';
+
 import { sync } from 'vuex-router-sync';
+
+import Mint from 'mint-ui';
+import 'mint-ui/lib/style.css';
 
 import store from './store';
 import router from './router';
 import App from './App';
 
-Vue.use(ElementUI, locale);
+// Vue.use(ElementUI, locale);
+Vue.use(Mint);
 
 Vue.config.productionTip = false;
 
