33c32b85b8fae6def0b429dc2194b37893e9e75f
Alexander Levin <alevin.ru@gmail.com>
2018-05-12 16:06:47 +0300

    signIn with one input

--- src/pages/SignInPage.vue
+++ src/pages/SignInPage.vue
@@ -8,22 +8,22 @@
   .fields
 
     mt-field(
+    v-if="phaState==='sms'"
     label="Телефон"
     v-model="phone"
-    :attr="{id: 'phone-input'}"
-    :placeholder="placeholder"
+    readonly
     )
 
     mt-field(
-    v-if="phaState==='sms'"
-    label="СМС-код"
-    v-model="sms"
-    type="number"
-    :attr="{id: 'sms-input'}"
+    v-model="input"
+    :label="label"
+    :attr="{id: 'sign-input'}"
+    :placeholder="placeholder"
     )
 
+
   .buttons
-    mt-button(type="primary" :disabled="!isComplete" v-on:click="sendClick") Отправить
+    mt-button(type="primary" :disabled="!isComplete" @click="sendClick") Отправить
 
 </template>
 <script>
@@ -32,7 +32,8 @@ import InputMask from 'inputmask';
 import { AUTH_REQUEST, AUTH_REQUEST_CONFIRM } from '@/store/auth/actions';
 
 const mask = '+7 (999) 999-99-99';
-const im = new InputMask(mask);
+const maskPhone = new InputMask(mask);
+const maskSms = new InputMask('9{4,6}');
 
 export default {
 
@@ -40,9 +41,10 @@ export default {
 
   data() {
     return {
+      input: '',
       phone: '',
       sms: '',
-      placeholder: mask.replace(/9/g, '_'),
+      // placeholder: mask.replace(/9/g, '_'),
       masked: {},
     };
   },
@@ -52,28 +54,64 @@ export default {
       return this.masked.isComplete && this.masked.isComplete();
     },
     phaState() {
-      return this.$store.state.auth.PHA_AUTH_ID ? 'sms' : 'phone';
+      return this.$store.state.auth.PHA_AUTH_ID && this.phone ? 'sms' : 'phone';
+    },
+    placeholder() {
+      return this.phaState === 'phone' ? mask.replace(/9/g, '_') : '4-6 цифр в СМС';
+    },
+    label() {
+      return this.phaState === 'phone' ? 'Телефон' : 'Код';
+    },
+  },
+
+  watch: {
+    phaState() {
+      this.attachMask();
     },
   },
 
   methods: {
     sendClick() {
-      if (this.phaState === 'phone') {
-        const phone = `8${this.masked.unmaskedvalue()}`;
-        return this.$store.dispatch(AUTH_REQUEST, phone);
+
+      if (!this.isComplete) {
+        return false;
       }
-      return this.$store.dispatch(AUTH_REQUEST_CONFIRM, this.sms);
+
+      const value = this.masked.unmaskedvalue();
+
+      if (this.phaState === 'phone') {
+        const phone = `8${value}`;
+        return this.$store.dispatch(AUTH_REQUEST, phone)
+          .then(() => {
+            this.phone = this.input;
+            this.input = '';
+          });
+      }
+
+      return this.$store.dispatch(AUTH_REQUEST_CONFIRM, value)
+        .then(() => this.$router.push('/'));
+
+    },
+    element() {
+      return document.getElementById('sign-input');
+    },
+    attachMask() {
+      const el = this.element();
+      if (!el) {
+        return;
+      }
+      const masker = this.phaState === 'phone' ? maskPhone : maskSms;
+      this.masked = masker.mask(el);
     },
   },
 
   mounted() {
+
     this.$nextTick(() => {
-      const el = document.getElementById('phone-input');
-      if (!el) {
-        return;
-      }
-      this.masked = im.mask(el);
+      this.attachMask();
+      this.element().onkeyup = e => e.which === 13 && this.sendClick();
     });
+
   },
 
 };
