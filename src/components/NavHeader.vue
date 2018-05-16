6536cf5e14b80c964350f1a5f36ca4cb7bb43892
Alexander Levin <alevin.ru@gmail.com>
2018-05-16 09:16:09 +0300

    nav-header component

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+
+.nav-header
+
+  mt-button.prev(:plain="true" @click="prevClick" :disabled="!prev")
+    i.el-icon-arrow-left(v-if="prev")
+
+  .title
+    strong {{ title }}
+
+  mt-button.prev(:plain="true" @click="nextClick" :disabled="!next")
+    i.el-icon-arrow-right(v-if="next")
+
+</template>
+<script>
+
+export default {
+
+  name: 'NavHeader',
+
+  props: {
+    title: String,
+    prev: Function,
+    next: Function,
+  },
+
+  methods: {
+    prevClick() {
+      this.prev();
+    },
+    nextClick() {
+      this.next();
+    },
+  },
+
+};
+
+</script>
+<style scoped lang="scss">
+
+@import "../styles/variables";
+
+
+</style>
--- src/config/global-components.js
+++ src/config/global-components.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
 import HelloWorld from '@/components/HelloWorld';
+import NavHeader from '@/components/NavHeader';
 
 Vue.component('HelloWorld', HelloWorld);
+Vue.component('NavHeader', NavHeader);
