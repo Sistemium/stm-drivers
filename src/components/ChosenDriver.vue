e1b964204b389a5a72f0c34a47fb0ed531228fb5
Alexander Levin <alevin.ru@gmail.com>
2018-05-20 14:39:08 +0300

    chosen-driver

---
+++ src/components/ChosenDriver.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+
+.chosen-driver
+
+  mt-cell(v-if="current" :title="current.name" label="В роли водителя")
+    mt-button(size="small" @click="chooseDriver(null)") Сменить
+
+  choose-driver(v-else)
+
+</template>
+<script>
+
+import { mapState, mapActions } from 'vuex';
+import { SET_CURRENT } from '@/store/driver';
+
+import ChooseDriver from './ChooseDriver';
+
+export default {
+
+  components: { ChooseDriver },
+  computed: mapState('driver', ['current']),
+  methods: mapActions('driver', { chooseDriver: SET_CURRENT }),
+
+};
+
+</script>
+<style scoped lang="scss">
+
+</style>
