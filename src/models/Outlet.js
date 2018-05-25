7e2444a79708ed873e2b9a87b7e0eaeca8cf81f8
Alexander Levin <alevin.ru@gmail.com>
2018-05-25 16:23:20 +0300

    missing Outlet relations

--- src/models/Outlet.js
+++ src/models/Outlet.js
@@ -1,17 +1,28 @@
 import STModel from '@/jsdata/Model';
 
 import './Partner';
+import './Shipment';
+import './ShipmentRoutePoint';
 
 export default new STModel({
 
   name: 'Outlet',
 
   relations: {
-    // TODO: fix jsData error on store.clear/removeAll if 'hasOne' instead of 'belongsTo'
     belongsTo: {
       Partner: {
         localKey: 'partnerId',
         localField: 'partner',
+      },
+    },
+    hasMany: {
+      Shipment: {
+        localField: 'shipments',
+        foreignKey: 'outletId',
+      },
+      ShipmentRoutePoint: {
+        localField: 'routePoints',
+        foreignKey: 'outletId',
       },
     },
   },
