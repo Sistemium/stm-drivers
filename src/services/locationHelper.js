72fa4b139766e67f6f73f6845a1f4547bf4103d7
Edgar Jan <krodanix@gmail.com>
2018-06-06 15:27:37 +0300

    locationHelper

---
+++ src/services/locationHelper.js
@@ -0,0 +1,112 @@
+import { serverDateTimeFormat } from '@/config/moments';
+import { get, assign } from 'lodash';
+import { isNative, checkIn } from './native';
+import Location from '../models/Location';
+
+export default function getLocation(desiredAccuracy, requiredAccuracy, ownerXid, target, timeout) {
+
+  const initData = {
+    ownerXid,
+    target,
+  };
+
+  if (isNative()) {
+
+    return checkIn(desiredAccuracy, requiredAccuracy, initData, timeout);
+
+  }
+
+  return browserGetLocation(desiredAccuracy, requiredAccuracy, timeout)
+    .then(location => Location.create(assign(location, initData)));
+
+}
+
+function browserGetLocation(desiredAccuracy, requiredAccuracy, timeout) {
+
+  const geo = window.navigator.geolocation;
+  const time = timeout || 30000;
+
+  return new Promise((resolve, reject) => {
+
+    const geoOptions = {
+      enableHighAccuracy: true,
+      maximumAge: timeout,
+      timeout: time,
+    };
+
+    const watchID = geo.watchPosition(successWatch, failWatch, geoOptions);
+
+    let savedCords;
+
+    const timeoutId = setTimeout(() => {
+
+      geo.clearWatch(watchID);
+
+      if (savedCords) {
+
+        const res = {
+          horizontalAccuracy: savedCords.accuracy,
+          latitude: savedCords.latitude,
+          longitude: savedCords.longitude,
+          timestamp: serverDateTimeFormat(Date()),
+        };
+
+        resolve(res);
+
+      } else {
+
+        reject('Время ожидания геометки истекло');
+
+      }
+
+    }, time);
+
+    function successWatch(location) {
+      if (get(location, 'coords.accuracy') <= desiredAccuracy) {
+        clearTimeout(timeoutId);
+        geo.clearWatch(watchID);
+        const coords = location.coords;
+        const res = {
+          horizontalAccuracy: coords.accuracy,
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+          timestamp: serverDateTimeFormat(Date()),
+        };
+
+        resolve(res);
+      } else if (get(location, 'coords.accuracy') <= requiredAccuracy &&
+        (!savedCords || savedCords.accuracy > get(location, 'coords.accuracy'))) {
+
+        savedCords = location;
+
+      }
+    }
+
+    function failWatch(err) {
+
+      clearTimeout(timeoutId);
+
+      geo.clearWatch(watchID);
+
+      if (savedCords) {
+
+        const res = {
+          horizontalAccuracy: savedCords.accuracy,
+          latitude: savedCords.latitude,
+          longitude: savedCords.longitude,
+          timestamp: serverDateTimeFormat(Date()),
+        };
+
+        resolve(res);
+
+      } else {
+
+        reject(err);
+
+      }
+
+    }
+
+  });
+
+}
