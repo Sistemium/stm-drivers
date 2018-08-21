1ec762f7e6a05ec30dc6df9c0a101737df5c7efc
Alexander Levin <alevin.ru@gmail.com>
2018-08-21 20:05:33 +0300

    ShipmentRoutePoint loadShipmentStats()

--- src/models/ShipmentRoutePoint.js
+++ src/models/ShipmentRoutePoint.js
@@ -1,10 +1,22 @@
+import Vue from 'vue';
+import flatten from 'lodash/flatten';
+import map from 'lodash/map';
+import filter from 'lodash/filter';
+import keyBy from 'lodash/keyBy';
+import sumBy from 'lodash/sumBy';
+
 import Model from '@/jsdata/Model';
+
+import ShipmentPosition from '@/models/ShipmentPosition';
 
 import './ShipmentRoute';
 import './Outlet';
 import './Location';
 import './ShipmentRoutePointShipment';
 import './ShipmentRoutePointPhoto';
+
+const positionsData = {};
+const statsByRoutePointId = {};
 
 export default new Model({
 
@@ -37,4 +49,39 @@ export default new Model({
     },
   },
 
+  methods: {
+
+    shipmentStats() {
+      const shipmentIds = keyBy(this.routePointShipments, 'shipmentId');
+      const stats = filter(positionsData, (stat, shipmentId) => shipmentIds[shipmentId]);
+      statsByRoutePointId[this.id] = statsByRoutePointId[this.id] || {};
+      const stat = statsByRoutePointId[this.id];
+      Vue.set(stat, 'positions', sumBy(stats, 'count()'));
+      Vue.set(stat, 'volume', sumBy(stats, 'sum(volume)'));
+      Vue.set(stat, 'count', stats.length);
+      return stat;
+    },
+
+  },
+
 });
+
+
+export async function loadShipmentStats(routePoints) {
+
+  const shipmentIds = flatten(routePoints.map(point => {
+    const { routePointShipments } = point;
+    return map(routePointShipments, 'shipmentId');
+  }));
+  const positionsFilter = {
+    where: {
+      shipmentId: { '==': shipmentIds },
+    },
+  };
+  const res = await ShipmentPosition.groupBy(positionsFilter, ['shipmentId']);
+
+  res.forEach(stat => {
+    positionsData[stat.shipmentId] = stat;
+  });
+
+}
